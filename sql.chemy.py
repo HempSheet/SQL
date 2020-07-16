c012043622d9d44ebcfe34e7356577b5c2dc74ca
HempSheet <58041457+HempSheet@users.noreply.github.com>
2020-07-16 14:56:30 +0300

    Create sql.chemy.py
    add

---
+++ sql.chemy.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, BigInteger, BOOLEAN, Date, Text, \
+   SMALLINT, ForeignKey
+
+engine = create_engine("mysql+pymysql://root:Rootpass12345@localhost/dbname", echo=True)
+meta = MetaData()
+
+users = Table(
+   'users', meta,
+   Column('id', Integer, primary_key=True),
+   Column('username', String(50)),
+   Column('password', String(255)),
+   Column('is_active', BOOLEAN),
+   Column('balance', BigInteger),
+)
+
+Session = Table(
+   'session', meta,
+   Column('id', String(50), ForeignKey('users.id')),
+   Column('user_id', Integer),
+   Column('created_date', Date),
+   Column('expired_date', Date),
+   Column('data', Text),
+   Column('ip', String(50)),
+   Column('user_agent', String(50)),
+)
+Transaction = Table(
+   'transaction', meta,
+   Column('id', Integer, ForeignKey('users.id')),
+   Column('created_date', Date),
+   Column('type_transaction', SMALLINT),
+   Column('amount', BigInteger),
+   Column('user_id', Integer),
+)
+meta.create_all(engine)
