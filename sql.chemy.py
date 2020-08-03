4e9fed4f6853abb7c373918ef99ab8c731423439
HempSheet <58041457+HempSheet@users.noreply.github.com>
2020-08-03 23:18:29 +0300

    Update sql.chemy.py
    add

--- sql.chemy.py
+++ sql.chemy.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, BigInteger, BOOLEAN, Date, Text, \
-   SMALLINT, ForeignKey
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, BigInteger,\
+   BOOLEAN, TIME, Text, SMALLINT, ForeignKey
 
 engine = create_engine("mysql+pymysql://root:Rootpass12345@localhost/dbname", echo=True)
 meta = MetaData()
@@ -17,8 +17,8 @@ Session = Table(
    'session', meta,
    Column('id', String(50), primary_key=True),
    Column('user_id', Integer, ForeignKey('users.id')),
-   Column('created_date', Date),
-   Column('expired_date', Date),
+   Column('created_date', TIME),
+   Column('expired_date', TIME),
    Column('data', Text),
    Column('ip', String(50)),
    Column('user_agent', String(50)),
@@ -26,9 +26,25 @@ Session = Table(
 Transaction = Table(
    'transaction', meta,
    Column('id', Integer, primary_key=True),
-   Column('created_date', Date),
+   Column('created_date', TIME),
    Column('type_transaction', SMALLINT),
    Column('amount', BigInteger),
    Column('user_id', Integer, ForeignKey('users.id')),
 )
 meta.create_all(engine)
+
+ins = users.insert()
+ins = users.insert().values(username='Ron', password='12345', balance=222)
+conn = engine.connect()
+result = conn.execute(ins)
+
+ins = Session.insert()
+ins = Session.insert().values(id=1, user_id=1, created_date='22:07:20',
+                              expired_date='23:07:20', data='today',
+                              ip='22.22.222.222', user_agent='1')
+
+ins = Transaction.insert()
+ins = Transaction.insert().values(id='2', created_date='22:07:20', type_transaction='2',
+                                  amount=222, user_id=1)
+conn = engine.connect()
+result = conn.execute(ins)
